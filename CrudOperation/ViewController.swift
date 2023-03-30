e3312338f00878c3d2b913b2dfd6fee6dc2a205c
enes öztürk <e.nesozturk@icloud.com>
2023-03-30 18:17:43 +0300

    Initial Commit

---
+++ CrudOperation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrudOperation
+//
+//  Created by enes öztürk on 30.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
